027d1847103873a1ae60ac25f4037bc60e5fb176
teamwaahid <teamwaahid@gmail.com>
2022-07-17 16:03:10 +0600

    dot bug solved

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const previousOperandElement = document.querySelector(".previous_operend");
-const currentOperandElement = document.querySelector(".current_operend");
-const equalButton = document.querySelector(".equal_btn");
-const acButton = document.querySelector("#ac");
-const delButton = document.querySelector("#del");
-const operatorButton = document.querySelectorAll(".operator");
-const numbersButton = document.querySelectorAll(".numbers");
+const previousOperandElement = document.querySelector('.previous_operend');
+const currentOperandElement = document.querySelector('.current_operend');
+const equalButton = document.querySelector('.equal_btn');
+const acButton = document.querySelector('#ac');
+const delButton = document.querySelector('#del');
+const operatorButton = document.querySelectorAll('.operator');
+const numbersButton = document.querySelectorAll('.numbers');
 
-let previousOperand = "";
-let currentOperand = "";
-let operators = "";
+let previousOperand = '';
+let currentOperand = '';
+let operators = '';
 
 function formattingNumber(num) {
-  return Number(num).toLocaleString("en");
+  return Number(num).toLocaleString('en');
 }
 
 function updateDisplay() {
@@ -32,27 +32,27 @@ function chooseOperator(item) {
     previousOperand = currentOperand;
   }
   operators = item;
-  currentOperand = "";
+  currentOperand = '';
 }
 
 function totalCalculation() {
   switch (operators) {
-    case "÷":
+    case '÷':
       return Number(previousOperand) / Number(currentOperand);
-    case "*":
+    case '*':
       return Number(previousOperand) * Number(currentOperand);
-    case "+":
+    case '+':
       return Number(previousOperand) + Number(currentOperand);
-    case "-":
+    case '-':
       return Number(previousOperand) - Number(currentOperand);
-    case "%":
+    case '%':
       return Number(previousOperand) % Number(currentOperand);
   }
 }
 
 // Operator Button
-operatorButton.forEach((btn) => {
-  btn.addEventListener("click", function () {
+operatorButton.forEach(btn => {
+  btn.addEventListener('click', function () {
     if (!currentOperand) return;
     chooseOperator(btn.textContent);
     updateDisplay();
@@ -60,37 +60,40 @@ operatorButton.forEach((btn) => {
 });
 
 // Equal Button
-equalButton.addEventListener("click", function () {
+equalButton.addEventListener('click', function () {
   if (!previousOperand) return;
   if (currentOperand) {
     currentOperand = totalCalculation();
   } else {
     currentOperand = previousOperand;
   }
-  previousOperand = "";
-  operators = "";
+  previousOperand = '';
+  operators = '';
   updateDisplay();
 });
 
 // Clear All Button
-acButton.addEventListener("click", function () {
-  previousOperand = "";
-  currentOperand = "";
-  operators = "";
+acButton.addEventListener('click', function () {
+  previousOperand = '';
+  currentOperand = '';
+  operators = '';
   updateDisplay();
 });
 
 // Del Button
-delButton.addEventListener("click", function () {
+delButton.addEventListener('click', function () {
   currentOperand = currentOperand.slice(0, -1);
   updateDisplay();
 });
 
 // ALL Number Button
-numbersButton.forEach((btn) => {
-  btn.addEventListener("click", function () {
-    if (btn.textContent === "." && currentOperand.includes(".")) return;
-    appendNumber(btn.textContent);
-    updateDisplay();
+numbersButton.forEach(btn => {
+  btn.addEventListener('click', function () {
+    if (btn.textContent === '.' && currentOperand.includes('.')) return;
+    else if (btn.textContent === '.' && currentOperand === '') return;
+    else {
+      appendNumber(btn.textContent);
+      updateDisplay();
+    }
   });
 });
